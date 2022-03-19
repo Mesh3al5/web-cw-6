0ea286699b7b6efc76bbba229cb199383186f1ff
Mesh3al5 <100314405+Mesh3al5@users.noreply.github.com>
2022-03-19 13:47:29 +0300

    cw-6

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,1 +1,28 @@
 // هنا سوف تنشئ دالة calculate 
+function calculate() {
+let height = document.getElementById("height").value;
+let weight =  document.getElementById("weight").value;
+
+if (height == "" || weight == "") {
+    alert(" تاكد من تعبئة جميع الحقول المطلوبة");
+    return;
+}
+
+let BMI = weight / (height * height);
+let status = "";
+
+if (BMI < 18.5) {
+    status =  "تعاني من نقص في الوزن";
+} else if (BMI < 25) {
+    status = "وزنك صحي"
+} else if (BMI < 30) {
+    status = "تعاني من زيادة في الوزن"
+} else {
+    status = "تعاني من السمنة المفرطة"
+}
+
+
+BMI = BMI.toFixed(2);
+document.getElementById("result").innerHTML = BMI;
+document.getElementsByClassName("comment")[0].innerHTML = status;
+}
---
+++ Exercise 1/ex1.js
@@ -0,0 +1,7 @@
+function travel(x) {
+    console.log("أود السفر الى كوكب " + x);
+}
+
+let planet = prompt("اسم الكوكب");
+
+travel(planet);
